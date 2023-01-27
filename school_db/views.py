d880c735c5a5bcd2130dfe4c3ae815f1999ec89b
Whomerding <wendy.homerding@gmail.com>
2023-01-27 13:21:04 -0500

    up to problem 6 complete

--- school_db/views.py
+++ school_db/views.py
@@ -143,8 +143,13 @@ SELECT `school_db_instructor`.`id`,
 # Print the instructors name and courses that he belongs to in the terminal
 # (Do not hard code his name in the print)
 def problem_three(request):
-
-    return complete(request)
+  instructors = Instructor.objects.get(pk=2)
+  
+  print (f"Instructor Name: {instructors.first_name} {instructors.last_name}")
+  courses = Course.objects.filter(instructor__pk = 2)
+  for course in courses:
+    print(f"Courses: -{course.name}")
+  return complete(request)
 
 
 # Supporting Query Method Documentation:
@@ -189,8 +194,13 @@ SELECT `school_db_instructor`.`id`,
 
 # Get the count of students, courses, and instructors and print them in the terminal
 def problem_four(request):
-
-    return complete(request)
+  students = Student.objects.count()
+  print (f"Students Count: {students}")
+  courses=Course.objects.count()
+  print (f"Courses Count: {courses}")
+  instructors=Instructor.objects.count ()
+  print (f"Instructors Count: {instructors}") 
+  return complete(request)
 
 
 # Supporting Query Method Documentation:
@@ -233,8 +243,9 @@ SELECT COUNT(*) AS `__count`
 # Print the new student's id, full name, year, and gpa to the terminal
 # NOTE every time you execute this function a duplicate student will be created with a different primary key number
 def problem_five(request):
-
-    return complete(request)
+  new_student = Student.objects.create(first_name="Wendy", last_name="Homerding", year=4, gpa=4.0)
+  print (f"ID: {new_student.pk}\nFull Name: {new_student.first_name} {new_student.last_name}\nYear: {new_student.year}\nGPA: {new_student.gpa}")    
+  return complete(request)
 
 
 # Supporting Query Method Documentation:
@@ -265,11 +276,14 @@ VALUES ('Kyle', 'Harwood', 9, 3.0)
 # Then query the studets table to get that student by their id
 # Print the new student's id, full name, and gpa to the terminal
 def problem_six(request):
-
+  new_student=Student.objects.filter(pk=11).update(gpa=3.95)
+  new_student=Student.objects.get(pk=11)
+  print (f"ID: {new_student.pk}\nFull Name: {new_student.first_name} {new_student.last_name}\nGPA: {new_student.gpa}")
     # Make sure to set this equal to the primary key of the row you just created!
-    student_id = 11
+  """student_id = 11"""
+  
 
-    return complete(request)
+  return complete(request)
 
 
 # Supporting Query Method Documentation:
